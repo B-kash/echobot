921e234943561164ff3e81dcd4627f90f9871118
bikash_chapagain <bikash.chapagain@theoplayer.com>
2020-06-28 21:48:59 +0200

    ✨ Add interpolate method to match strings

--- assignments/interpolateSession/src/interpolate.js
+++ assignments/interpolateSession/src/interpolate.js
@@ -1,5 +1,20 @@
 const interpolate = (value, session = {}, options = {}) => {
-    //TODO
+    const fallbackRegexString = options.leftDelimiter + '[^' + options.rightDelimiter + ']*' + options.rightDelimiter;
+    const fallbackRegex = new RegExp(fallbackRegexString);
+    let keys = Object.keys(session);
+    if (keys.length === 0) {
+        value = value.replace(fallbackRegex, '');
+    }
+    for (const key of keys) {
+        const regexString = options.leftDelimiter + key + options.rightDelimiter;
+        const regex = new RegExp(regexString);
+        if (value.match(regex)) {
+            value = value.replace(regex, session[key]);
+        } else {
+            value = value.replace(fallbackRegex, '');
+        }
+    }
+    return value;
 };
 
-module.exports = { interpolate }
+module.exports = {interpolate};
