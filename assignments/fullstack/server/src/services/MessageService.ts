ba947abaaea36b9004fd3676bec6729a393d4e79
bikash_chapagain <bikash.chapagain@theoplayer.com>
2020-06-28 20:48:52 +0200

    ✨ Add welcome message

--- assignments/fullstack/server/src/services/MessageService.ts
+++ assignments/fullstack/server/src/services/MessageService.ts
@@ -2,12 +2,17 @@ import {Message} from "../model/Message";
 
 export class MessageService {
 
-    constructor() {
-    }
-
     public readAndRespondMessage(message: Message): Message {
         message.seenTime = new Date();
         // Later on this message can be saved to the DB if need be
         return message;
+    }
+
+    public welcomeMessage(): Message {
+        return {
+            text: 'Welcome to echo bot',
+            sentTime: new Date(),
+            seenTime: null,
+        };
     }
 }
