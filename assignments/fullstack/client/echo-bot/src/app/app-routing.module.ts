6527cb92a88e9284488e40c27466821b07a24c74
bikash_chapagain <bikash.chapagain@theoplayer.com>
2020-06-29 01:28:10 +0200

    ♻️ Add routing and update components

--- assignments/fullstack/client/echo-bot/src/app/app-routing.module.ts
+++ assignments/fullstack/client/echo-bot/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {ChatComponent} from "./chat/chat.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'chat', component: ChatComponent
+  }, {
+    path: '', pathMatch: 'full', redirectTo: 'chat'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- assignments/fullstack/client/echo-bot/src/app/app.module.ts
+++ assignments/fullstack/client/echo-bot/src/app/app.module.ts
@@ -3,12 +3,18 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HistoryComponent} from './history/history.component';
+import {MessageBoxComponent} from './message-box/message-box.component';
+import {ChatComponent} from './chat/chat.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HistoryComponent,
+    MessageBoxComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
