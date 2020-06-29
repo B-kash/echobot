1d4be9f274a29faaf82239fa5b0339ce884ad2cf
bikash_chapagain <bikash.chapagain@theoplayer.com>
2020-06-29 02:55:53 +0200

    ♻️ update interpolate logic to interpolate all occurance of a word

--- assignments/interpolateSession/src/interpolate.js
+++ assignments/interpolateSession/src/interpolate.js
@@ -1,13 +1,13 @@
 const interpolate = (value, session = {}, options = {}) => {
     const fallbackRegexString = options.leftDelimiter + '[^' + options.rightDelimiter + ']*' + options.rightDelimiter;
-    const fallbackRegex = new RegExp(fallbackRegexString);
+    const fallbackRegex = new RegExp(fallbackRegexString, 'g');
     let keys = Object.keys(session);
     if (keys.length === 0) {
         value = value.replace(fallbackRegex, '');
     }
     for (const key of keys) {
         const regexString = options.leftDelimiter + key + options.rightDelimiter;
-        const regex = new RegExp(regexString);
+        const regex = new RegExp(regexString, 'g');
         if (value.match(regex)) {
             value = value.replace(regex, session[key]);
         } else {
